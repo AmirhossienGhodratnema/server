60382e07d17489fd7c3c5a8d484a445db1fa312a
AmirhosseinGhodratnema <amirhosseinghodratnema@gmail.com>
2021-12-14 21:47:13 +0330

    check sharp !!!!!!!!!!!!!!1

--- app/http/controllers/admin/courseController.js
+++ app/http/controllers/admin/courseController.js
@@ -1,10 +1,13 @@
 // Require
 const Controller = require('./../controller')
+const fs = require('fs');
+const path = require('path')
+const sharp = require('sharp');
 
 
 // Models
 const Course = require('app/models/courses');
-const fs = require('fs');
+
 
 
 // Helper Upload Image
@@ -20,10 +23,19 @@ module.exports = new class CourseController extends Controller {
 
     async store(req, res, next) {
         let result = await this.validationData(req);
+
+        console.log(req.body)
+
+
+        if (req.file) {
+            fs.unlink(req.file.path, (err) => { })
+        };
+
         if (result) {
 
             let images = req.body.images;
             let { title, type, body, tags, price, slug } = req.body;
+
 
             let newCourse = new Course({
                 user: req.user._id,
@@ -36,24 +48,35 @@ module.exports = new class CourseController extends Controller {
                 price
             });
 
-
-
             await newCourse.save(err => {
                 if (err) throw err;
             });
 
             return res.redirect('/admin/courses');
-
-
         };
 
-        // Delete upload file with error.
-        if(req.file) {
-            fs.unlink(req.file.path, (err) => {})
-        }
         return this.back(req, res);
 
     };
+
+    imageResize(image) {
+        let imageInfo = path.parse(image.path);
+
+        let addresImage = {}
+        addresImage['original'] = this.getUrlImage(image.destination, image.originalname);
+
+
+
+        return addresImage;
+
+    }
+
+    getUrlImage(dir, name) {
+        return dir.substring(12) + '/' + name
+    }
+
+
+
 
     slug(title) {
         return title.replace(/([^۰-۹آ-یa-z0-9]|-)+/g, "-")
