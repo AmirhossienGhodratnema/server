0a8557654bffc9ff5fb6de5ca10635cad44356cb
AmirhosseinGhodratnema <amirhosseinghodratnema@gmail.com>
2021-12-20 13:28:19 +0330

    View development ok

--- app/routers/errorHandler.js
+++ app/routers/errorHandler.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.all('*', async (req, res, next) => {
     try {
+        res.statusCode = 404
         throw new Error()
     } catch (err) {
         next(err);
@@ -12,6 +13,7 @@ router.all('*', async (req, res, next) => {
 
 router.use((err, req, res, next) => {
 
+    let statusCode = res.statusCode || 500;
     let errorMasaage = 'چنین صفحه ای یافت نشد' || '';
     let stack = err.stack || '';
 
@@ -21,7 +23,10 @@ router.use((err, req, res, next) => {
         extractStyle: false,
     };
 
-    res.render('error/stack', { ...layouts, errorMasaage, stack });
+    console.log('statusCode :', statusCode)
+    if (config.debuge) res.render('error/stack', { ...layouts, errorMasaage, stack });
+
+    res.render(`error/${statusCode}`, { ...layouts, errorMasaage, stack });
 });
 
 
