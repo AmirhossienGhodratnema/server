f2ec08dd11b503fdacca270e657a7fba3d295540
AmirhosseinGhodratnema <amirhosseinghodratnema@gmail.com>
2022-01-01 22:16:01 +0330

    Edite Episode fix

--- app/http/controllers/admin/episodeController.js
+++ app/http/controllers/admin/episodeController.js
@@ -18,12 +18,14 @@ module.exports = new class EpisodesController extends Controller {
     async index(req, res) {
         try {
             let page = req.query.page || 1
+            let courses = await Course.find({})
             let episodes = await Episodes.paginate({}, { page, sort: { createdAt: -1 }, limit: 10 })
-            res.render('admin/episode/index', { title: 'جلسات دوره', episodes });
+            res.render('admin/episode/index', { title: 'جلسات دوره', episodes, courses });
         } catch (err) {
             next(err)
         }
     };
+
 
     async create(req, res, next) {
         try {
@@ -37,27 +39,18 @@ module.exports = new class EpisodesController extends Controller {
     async store(req, res, next) {
         try {
             let result = await this.validationData(req);
-
             if (result) {
-
-                let { title, course, videoUrl, time, body, type, number } = req.body;
-                // console.log(...req.body)
                 let newEpisode = new Episodes({ ...req.body });
-
                 await newEpisode.save(err => {
                     if (err) throw err;
                 });
-
                 return res.redirect('/admin/episode');
             };
-
             return this.back(req, res);
-
         } catch (err) {
             next(err)
         }
     };
-
 
 
     async distroy(req, res, next) {
@@ -67,8 +60,6 @@ module.exports = new class EpisodesController extends Controller {
                 req.flash('massage', 'چنین دوره ای وجود ندارد');
                 console.log('Not Course')
             }
-
-
             episode.remove()
             res.redirect('/admin/episode');
         } catch (err) {
@@ -81,9 +72,7 @@ module.exports = new class EpisodesController extends Controller {
     async edit(req, res, next) {
         try {
             this.isMongoId(req.params.id);
-
             let episode = await Episodes.findOne({ _id: req.params.id })
-
             if (!episode) {
                 throw new Error('چنین دوره ای وجود ندارد')
             } else {
@@ -98,30 +87,11 @@ module.exports = new class EpisodesController extends Controller {
     async update(req, res, next) {
         try {
             let status = await this.validationData(req);
-
-            let objUpdateData = {}
-
-            if (!status) {
-                if (req.file && req.flash('massage').length > 0) {
-                    return fs.unlink(req.file.path, (err) => { })
-                };
-            };
-
             if (!status) {
                 return this.back(req, res)
             };
-
-
-            if (req.file) {
-                objUpdateData.images = this.imageResize(req.file);
-                objUpdateData.fingerImage = objUpdateData.images['480']
-            }
-
-
-            await Course.findByIdAndUpdate(req.params.id, { $set: { ...req.body, ...objUpdateData } })
-            return res.redirect('/admin/courses')
-
-
+            await Episodes.findByIdAndUpdate(req.params.id, { $set: { ...req.body } })
+            return res.redirect('/admin/episode')
         } catch (err) {
             next(err)
         }
--- app/validation/episodeValidation.js
+++ app/validation/episodeValidation.js
@@ -5,13 +5,26 @@ const path = require('path');
 module.exports = new class EpisodeValidation extends validation {
     handel() {
         return [
-            // check('title')
-            //     .isLength({ min: 5 })
-            //     .withMessage('عنوان نمیتواند کم تر از 5 کاراکتر باشد'),
+            check('title')
+                .isLength({ min: 5 })
+                .withMessage('عنوان نمیتواند کم تر از 5 کاراکتر باشد'),
 
-            // check('body')
-            //     .isLength({ min: 20 })
-            //     .withMessage('متن دوره نمیتواند کمتر از 20 کاراکتر باشد'),
+            check('body')
+                .isLength({ min: 20 })
+                .withMessage('متن دوره نمیتواند کمتر از 20 کاراکتر باشد'),
+
+
+            check('videoUrl')
+                .not().isEmpty()
+                .withMessage('آدرس ویدیو الزامی است'),
+
+            check('time')
+                .not().isEmpty()
+                .withMessage('زمان را وارد کنید'),
+
+            check('number')
+                .not().isEmpty()
+                .withMessage('عدد دوره را وارد کنید'),
 
             check('type')
                 .not().isEmpty()
