b19e92b2a69bbc386d4666df419f8384d0402126
amirhossien <amirhosseinghodratnema@gmail.com>
2022-02-17 13:04:14 +0330

    format funcion is change

--- app/middleware/redirectIfNotAdmin.js
+++ app/middleware/redirectIfNotAdmin.js
@@ -1,12 +1,9 @@
 const middleware = require("./middleware");
 const user = require("app/models/user");
 
-module.exports = new (class RedirectIfNotAdmin extends middleware {
+module.exports = new class RedirectIfNotAdmin extends middleware {
   handel(req, res, next) {
-    console.log("req.isAuthenticated()", req.isAuthenticated());
-    console.log("req.user.admin", req.user.admin);
-
     if (req.isAuthenticated() && req.user.admin) return next();
     res.redirect("/");
   }
-})();
+};
