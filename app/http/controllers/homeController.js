ef476374acbc20915513327610a053141f176851
amirhossien <amirhosseinghodratnema@gmail.com>
2022-02-17 13:03:01 +0330

    scdfgdsfbgdsfbd

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,17 +1,17 @@
 // Require
-const Controller = require('./controller');
+const Controller = require("./controller");
 
-const Course = require('app/models/courses');
-const Episodes = require('app/models/episodes');
-const User = require('app/models/user');
+const Course = require("app/models/courses");
+const Episodes = require("app/models/episodes");
+const User = require("app/models/user");
 
-module.exports = new class HomeController extends Controller {
-    // Get home page view
-    async index(req, res ,next) {
-        try {
-            res.render('home/index',);         // Render home.ejs file
-        } catch (err) {
-            next(err);
-        }
-    };
-};
+module.exports = new (class HomeController extends Controller {
+  // Get home page view
+  async index(req, res, next) {
+    try {
+      res.render("home/index"); // Render home.ejs file
+    } catch (err) {
+      next(err);
+    }
+  }
+})();
--- app/routers/web/admin/admin.js
+++ app/routers/web/admin/admin.js
@@ -1,31 +1,24 @@
 // Require
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 router.use((req, res, next) => {
-    res.locals.layout = 'admin/master';
-    next()
+  res.locals.layout = "admin/master";
+  next();
 });
 
-
 // Require controller
-const AdminController = require('app/http/controllers/admin/adminController');
-
+const AdminController = require("app/http/controllers/admin/adminController");
 
 // Require Routers
-const courses = require('./courses');
-const episodes = require('./episodes');
-
-
+const courses = require("./courses");
+const episodes = require("./episodes");
 
 // Middleware
-router.use('/courses', courses)
-router.use('/episode', episodes)
-
+router.use('/courses', courses);
+router.use("/episode", episodes);
 
 // Route
-router.get('/', AdminController.index);
-
+router.get("/", AdminController.index);
 
 module.exports = router;
