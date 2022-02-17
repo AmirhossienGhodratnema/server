9d5a2345db83db808068c7477186b769b46d53eb
amirhossien <amirhosseinghodratnema@gmail.com>
2022-02-17 12:47:50 +0330

    Save and fix file is okay and uploadin test

--- app/middleware/redirectFauthenticated.js
+++ app/middleware/redirectFauthenticated.js
@@ -1,12 +1,12 @@
-const middleware = require('./middleware')
+const middleware = require("./middleware");
 
-// 
-module.exports = new class RedirectFauthentucated extends middleware {
-    // Filter login and registration routes for the user 
-    handel(req, res, next) {
-        if (req.isAuthenticated()) {
-            return res.redirect('/')
-        }
-        next();
-    };
-};
+//
+module.exports = new (class RedirectFauthentucated extends middleware {
+  // Filter login and registration routes for the user
+  handel(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/");
+    }
+    next();
+  }
+})();
--- app/routers/web/admin/courses.js
+++ app/routers/web/admin/courses.js
@@ -1,37 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
-
-
 // Upload file
-const upload = require('app/hellper/uploadimage');
-
+const upload = require("app/hellper/uploadimage");
 
 // Controller
-const courseController = require('app/http/controllers/admin/courseController');
-
+const courseController = require("app/http/controllers/admin/courseController");
 
 // Validation
-const CourseCreateValidation = require('app/validation/CourseCreateValidation');
+const CourseCreateValidation = require("app/validation/CourseCreateValidation");
 
+const convertFileToField = require("app/middleware/convertFileToField");
 
-const convertFileToField = require('app/middleware/convertFileToField')
+router.get("/", courseController.index);
 
-router.get('/', courseController.index);
+router.get("/create", courseController.create);
+router.post(
+  "/create",
+  upload.single("images"),
+  convertFileToField.handel,
+  CourseCreateValidation.handel(),
+  courseController.store
+);
 
-router.get('/create', courseController.create);
-router.post('/create', upload.single('images'), convertFileToField.handel, CourseCreateValidation.handel(), courseController.store);
+router.delete("/:id", upload.single("images"), courseController.distroy);
 
-router.delete('/:id', upload.single('images'), courseController.distroy);
-
-router.get('/:id/edit', courseController.edit);
-router.put('/:id', upload.single('images'), convertFileToField.handel, CourseCreateValidation.handel(), courseController.update);
-
-
-
+router.get("/:id/edit", courseController.edit);
+router.put(
+  "/:id",
+  upload.single("images"),
+  convertFileToField.handel,
+  CourseCreateValidation.handel(),
+  courseController.update
+);
 
 // edit
 
-module.exports = router; 
+module.exports = router;
